b8ba940813dc1c49e58c243cdaad8c44ff686bac
Gadzbi <crg1234@gmail.com>
2022-04-02 13:55:43 +0200

    First class in project

---
+++ .idea/Kurs.java
@@ -0,0 +1,17 @@
+/**
+ * Klasa zawierająca informacje o kursach autobusów
+ * @author Kacper Nitkiewicz
+ * @version  1.0
+ *
+ */
+public class Kurs{
+    long ID;
+    String godzinaRozpoczecia;
+    String godzinaZakonczenia;
+    float iloscSpalonegoPaliwa;
+    float iloscSkakowanychBiletow;
+
+    public int ObliczSumeOpoznien(){
+        return 0;
+    }
+}
