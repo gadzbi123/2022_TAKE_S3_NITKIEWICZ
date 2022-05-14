4eafdbb2b7d4a214527dc59a21029ffcc064bc9d
Rafał Klinowski <klinowskirafal@gmail.com>
2022-05-14 17:28:48 +0200

    Aktualizacja gitignore. Zmieniono nazwy pól wewnątrz klas. Dodano adnotacje związane z encjami do klas - utworzono relacje między klasami. Przeciążono metody toString, equals i getHashCode w klasach reprezentujących encje. Dodano metodę main, korzystającą z EntityManagerFactory do stworzenia przykładowego obiektu i umieszczenia go na serwerze. Naprawiono błąd związany z niewykrywaniem przez środowisko plików źródłowych. Dodano niezbędne biblioteki i ustawienia połączenia z bazą danych.

---
+++ src/pl/polsl/autobusy/entities/Autobus.java
@@ -0,0 +1,99 @@
+package pl.polsl.autobusy.entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * Klasa zawierająca informacje detaliczne o Autobusie
+ * @author Kacper Nitkiewicz
+ * @version  1.1
+ *
+ */
+
+@Entity
+public class Autobus{
+    @Id
+    @GeneratedValue(strategy= GenerationType.TABLE)
+    private long AutobusID;
+    private int Numer;
+    private String NumerRejestracyjny;
+    private int LiczbaMiejsc;
+    private boolean CzySprawny;
+
+    @OneToMany(cascade=CascadeType.ALL,fetch=FetchType.EAGER,orphanRemoval=true)
+    private List<Przejazd> Przejazdy;
+
+    public long getAutobusID() {
+        return AutobusID;
+    }
+
+    public void setAutobusID(long autobusID) {
+        this.AutobusID = autobusID;
+    }
+
+    public int getNumer() {
+        return Numer;
+    }
+
+    public void setNumer(int numer) {
+        Numer = numer;
+    }
+
+    public String getNumerRejestracyjny() {
+        return NumerRejestracyjny;
+    }
+
+    public void setNumerRejestracyjny(String numerRejestracyjny) {
+        NumerRejestracyjny = numerRejestracyjny;
+    }
+
+    public int getLiczbaMiejsc() {
+        return LiczbaMiejsc;
+    }
+
+    public void setLiczbaMiejsc(int liczbaMiejsc) {
+        LiczbaMiejsc = liczbaMiejsc;
+    }
+
+    public boolean isCzySprawny() {
+        return CzySprawny;
+    }
+
+    public void setCzySprawny(boolean czySprawny) {
+        CzySprawny = czySprawny;
+    }
+
+    public List<Przejazd> getPrzejazdy() {
+        return Przejazdy;
+    }
+
+    public void setPrzejazdy(List<Przejazd> przejazdy) {
+        Przejazdy = przejazdy;
+    }
+
+    @Override
+    public String toString() {
+        return "Autobus{" +
+                "ID=" + AutobusID +
+                ", Numer=" + Numer +
+                ", NumerRejestracyjny='" + NumerRejestracyjny + '\'' +
+                ", LiczbaMiejsc=" + LiczbaMiejsc +
+                ", CzySprawny=" + CzySprawny +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Autobus autobus = (Autobus) o;
+
+        return AutobusID == autobus.AutobusID;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (AutobusID ^ (AutobusID >>> 32));
+    }
+}
